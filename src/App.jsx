fcaf33323d387773b6a9dc6bf54f9c42dac72bcd
unknown <omoyeleolatuyole39@gmail.com>
2023-10-23 22:03:04 +0100

    created custom layouts for webapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+ 
+<section className="h-screen flex text-center">
+    <div className="w-1/4 bg-red-500 h-full">01</div>
+    <div className="w-1/2 bg-green-500 h-full">02</div>
+    <div className="w-1/4 bg-yellow-500 h-full">03</div>
+</section>
 
-export default App
+
+// if the tailwind isnt running on your pc simply install the dependency
+
+
+// In order for the tailwind css to run on your pc simply do the following 
+// 1. npm install -D tailwindcss postcss autoprefixer
+// 2. tailwindcss init -p
+
+//3. (In the tailwing.config.js paste this )
+
+//** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+//step 4. (in the index.css file paste this) 
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
+
+//step 5. npm run dev 
+
+
+
+    )
+}
+export default App;
+
