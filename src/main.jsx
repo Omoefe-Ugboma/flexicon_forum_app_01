c4442e88f8b935f5fb445ade231adb2de3d2772a
oneilh <obidiasodumebi@mail.com>
2023-11-25 09:50:24 +0100

    initiated the react-hot-toast package, made a toast based on success and error status of the form

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,36 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
-
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <div>
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              success: {
+                style: {
+                  border: "1px solid #21da30",
+                  padding: "2rem",
+                  color: "#fff",
+                  background: "#21da3058",
+                },
+              },
+              error: {
+                style: {
+                  border: "1px solid #ff0000",
+                  padding: "1rem 2rem",
+                  color: "#fff",
+                  background: "#ff000057",
+                  
+                },
+              },
+            }}
+          />
+        </div>
         <App />
       </BrowserRouter>
     </Provider>
