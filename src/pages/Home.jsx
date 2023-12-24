daf416780d640344b203efbe476e863ebae607c1
oneilh <obidiasodumebi@mail.com>
2023-12-24 10:47:19 +0100

    removed all layout and contents from homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,7 @@
-import SideBar from "../components/SideBar";
-import CreatePosts from "../features/create_posts/CreatePost";
-import Posts from "../features/posts/Posts";
-
 function Home() {
   return (
     <>
-      <section className="h-screen flex gap-4">
-        <div className="w-1/4 bg-gray-500 h-full">
-          {" "}
-          <SideBar />{" "}
-        </div>
-        {/* bg-gray-200 */}
-        <div className="w-1/2  h-full">
-          <CreatePosts />
-          <Posts />
-        </div>
-        <div className="w-1/4 bg-gray-500 h-full">03</div>
-      </section>
+      <p>Hello</p>
     </>
   );
 }
